c63302659cd99426022edbc218a0b0205d813315
tylergan <tgan2615@gmail.com>
2026-01-28 07:31:12 +1100

    included an iterative BFS for the tree height question since the level-look-ahead technique is valuable in other problems

--- binary_tree/max_depth_binary_tree.go
+++ binary_tree/max_depth_binary_tree.go
@@ -31,3 +31,50 @@ func maxDepth(root *TreeNode) int {
 
 	return 1 + max(leftHeight, rightHeight)
 }
+
+type Deque struct {
+	q []*TreeNode
+}
+
+func NewDeque() Deque {
+	return Deque{q: []*TreeNode{}}
+}
+
+func (d *Deque) Len() int { return len(d.q) }
+
+func (d *Deque) Enqueue(node *TreeNode) {
+	d.q = append(d.q, node)
+}
+
+func (d *Deque) Dequeue() *TreeNode {
+	if len(d.q) == 0 {
+		return nil
+	}
+	first := d.q[0]
+	d.q = d.q[1:]
+	return first
+}
+
+func maxDepthWithBFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	deque := NewDeque()
+	deque.Enqueue(root)
+	level := 0
+	for deque.Len() > 0 {
+		sz := deque.Len()
+		for i := 0; i < sz; i++ {
+			currNode := deque.Dequeue()
+			if currNode.Left != nil {
+				deque.Enqueue(currNode.Left)
+			}
+			if currNode.Right != nil {
+				deque.Enqueue(currNode.Right)
+			}
+		}
+		level++
+	}
+	return level
+}
